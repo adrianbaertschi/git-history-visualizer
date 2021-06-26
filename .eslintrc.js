3cdbfdb7e0002d98276838bb6447f5473083d0f1
adrianbaertschi <adrian.baertschi@zuehlke.com>
2021-06-26 14:39:35 +0800

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: 'standard-with-typescript',
+    parserOptions: {
+        project: './tsconfig.eslint.json',
+    }
+};
