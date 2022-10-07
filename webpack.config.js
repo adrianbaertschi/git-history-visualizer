25bb44dbcd9b1778df9c69829cbaf2415055964a
adrianbaertschi <adrian.baertschi@zuehlke.com>
2022-10-07 09:36:52 +0200

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
+      favicon: 'public/favicon.png'
     }),
     new NodePolyfillPlugin(),
     new VueLoaderPlugin(),
